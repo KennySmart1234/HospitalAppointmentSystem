11d284bb3e145b0a7b9a5ea41872f037a0912df7
CodedTonye <tonyefredhorsfall101@gmail.com>
2026-09-14 22:04:27 +0100

    AppointmentRequest commit

---
+++ src/main/java/com/hospitalAppointmentSystem/dtos/requests/AppointmentRequest.java
@@ -0,0 +1,52 @@
+package com.hospitalAppointmentSystem.dtos.requests;
+
+
+import com.hospitalAppointmentSystem.data.models.AppointmentStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AppointmentRequest {
+
+    public static class BookAppointmentRequest{
+
+        @NotBlank
+        private String department;
+
+        @NotBlank
+        private String description;
+
+        @NotNull
+        private LocalDateTime appointmentDatetime;
+    }
+
+    public static class ChangeAppointmentStatusRequest{
+
+        @NotNull
+        private AppointmentStatus status;
+    }
+
+    public static class AdminBookAppointmentRequest{
+
+        @NotNull
+        private UUID patientId;
+
+        @NotBlank
+        private String department;
+
+        @NotBlank
+        private String description;
+
+        @NotNull
+        private LocalDateTime appointmentDatetime;
+    }
+}
